90f85fcf9240ca8d69a59a14b0292133b59e1a08
Jennifer Hurd <jenhurd8@gmail.com>
2018-10-20 17:30:55 -0500

    up to section 7.15

--- src/main/java/com/example/android/justjava/MainActivity.java
+++ src/main/java/com/example/android/justjava/MainActivity.java
@@ -24,11 +24,21 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     int quantity = 1;
-//    int price = 5;
 
-    private String createOrderSummary(int price, boolean addWhippedCream){
+    //int price = 5;
+/*
+Create summary of the order
+*
+*@param addWhippedCream is whether or not the user wants whipped cream
+*@param addChocolate is where or not the user wants chocolate topping
+*@param price of the order
+*@return text summary
+*/
+
+    private String createOrderSummary(int price, boolean addWhippedCream, boolean addChocolate){
         String priceMessage = "Name: Kaptain Kunal";
         priceMessage += "\nAdd whipped cream? " + addWhippedCream;
+        priceMessage += "\nAdd chocolate? " + addChocolate;
          priceMessage +=  "\nQuantity: " + quantity;
          priceMessage += "\nTotal: $" + price;
         priceMessage +=  "\nThank you!";
@@ -37,12 +47,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void submitOrder(View view) {
         CheckBox whippedCreamCheckBox = (CheckBox)findViewById(R.id.whipped_cream_checkbox);
+        CheckBox hasChocolateCheckBox = (CheckBox)findViewById(R.id.has_chocolate_checkbox);
 
         boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+        boolean hasChocolate = hasChocolateCheckBox.isChecked();
         Log.v("MainActivity", "Has whipped cream: " + hasWhippedCream);
 
-        int price = calculatePrice();
-        String priceMessage = createOrderSummary(price, hasWhippedCream);
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
+        String priceMessage = createOrderSummary(price, hasWhippedCream, hasChocolate);
         displayMessage(priceMessage);
     }
 
@@ -59,12 +71,22 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Calculates the price of the order.
      *
-     * @return total price
+     *@param addWhippedCream is whether or not the user wants whipped cream topping
+     *@param addChocolate is where or not the user wants chocolate topping
+     *@return total price
      */
 
-    private int calculatePrice() {
-       int price = quantity * 5;
-       return price;
+    private int calculatePrice(boolean addWhippedCream, boolean addChocolate) {
+        int basePrice = 5;
+        if(addWhippedCream){
+            basePrice = basePrice++;
+        }
+        if(addChocolate){
+            basePrice += 2;
+        }
+
+       return quantity * basePrice;
+
     }
 
     /**
